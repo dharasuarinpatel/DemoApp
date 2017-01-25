54f426715ff405928f45883d8c09aba1c2bdc99d
dharasuarinpatel <dhara.patel48@mavs.uta.edu>
2017-01-25 17:43:37 -0500

    Update app.js
    Total functionality

--- public/app.js
+++ public/app.js
@@ -5,43 +5,71 @@ app.controller('myCtrl', ['$scope','$http','$log', function($scope, $http, $log)
 
 $scope.locations=["CYCLE","SILVER SNEAKERS","ALL CLASSES"];
 $scope.selectedLocation="";
- $scope.students=[];
+$scope.count=0;
+
     $scope.onSelectedLocation = function(l) {
-       $scope.selectedLocation=l;
-        console.log(l,$scope.selectedLocation);
-         if(l == "ALL CLASSES")
-       {
-           $scope.filteredData=false;
-           $scope.allData=true;
-           $scope.totalCount=true;
-           $scope.filterCount=false;
-       }
-       else{
-           $scope.filteredData=true;
-           $scope.allData=false;
-           $scope.filterCount=true;
-           $scope.totalCount=false;
-       }
+        $scope.students=[];
+        $scope.result=[];
+        $scope.selectedLocation=l;
+
          $http({
-            method : 'GET',
-            headers: {'X-Parse-Application-Id':'LTOTE2KzdaQcrKO0bhanuRROLHxjjxzy8URFzL4U'},
-            url : 'https://web.alariscloud.com/parse/classes/Dashboard'
-            }) 
+             method : 'GET',
+             headers: {'X-Parse-Application-Id':'LTOTE2KzdaQcrKO0bhanuRROLHxjjxzy8URFzL4U'},
+             url : 'https://web.alariscloud.com/parse/classes/Dashboard'
+         })
         .then(function (response) {
-            $scope.students = response.data.results;
-            $log.log($scope.students);
-            $scope.displayCount=true;  
-            $scope.count=$scope.students.length; 
-            },function(err){
+             $scope.result = response.data.results;
+             if ($scope.selectedLocation == "ALL CLASSES"){
+                    $scope.students=$scope.result;
+                    $scope.count=$scope.students.length;
+                    $log.log($scope.students);
+                }
+             else{
+                    var i,j = 0;
+                    for(i = 0; i < $scope.result.length; i++){
+                    if ($scope.result[i].program.name == $scope.selectedLocation)
+                        {
+                            $scope.students[j] = $scope.result[i];
+                            $log.log($scope.students[j]);
+                            j++;
+                        }
+                    }
+                    $scope.count=$scope.students.length;
+                }
+        $scope.displayCount=true; 
+        },function(err){
                 $scope.error = err.statusText || "Request Failed!";
                 alert("Error" + err.status + err.statusText + $scope.error);
                 alert(err);
-            });
+        });
+
     };
-     $scope.isActive = function(l) {
-        return $scope.selectedLocation === l;
- };
-   
+
+
 }]);
 
+app.directive('noImage', function () {
 
+    var setDefaultImage = function (el) {
+        el.attr('src', "/images/photo_not_available.png");
+    };
+
+    return {
+        restrict: 'A',
+        link: function (scope, el, attr) {
+            scope.$watch(function() {
+                return attr.ngSrc;
+            }, function () {
+                var src = attr.ngSrc;
+
+                if (!src) {
+                    setDefaultImage(el);
+                }
+            });
+
+            el.bind('error', function() { setDefaultImage(el); });
+        }
+    };
+});
+
+       
